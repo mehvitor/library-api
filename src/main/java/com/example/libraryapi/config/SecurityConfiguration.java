267ea95ad88cd94b0c4005d7193aba1f7754f216
Vitor Telles Escame <mehvitor93@gmail.com>
2025-11-26 15:51:00 -0300

    Criando estrutura de usuarios no BD e implementando o cadastro de usuarios

--- src/main/java/com/example/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/com/example/libraryapi/config/SecurityConfiguration.java
@@ -2,10 +2,17 @@ package com.example.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @Configuration
@@ -18,12 +25,40 @@ public class SecurityConfiguration {
 				.csrf(AbstractHttpConfigurer::disable)
 				.httpBasic(Customizer.withDefaults())
 				.formLogin(configurer ->{
-					configurer.loginPage("/login").permitAll();
+					configurer.loginPage("/login");
 				})
 				.authorizeHttpRequests(authorize -> {
+					authorize.requestMatchers("/login").permitAll();
+					authorize.requestMatchers("/autores/**").hasRole("ADMIN");
+					authorize.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
+					
 					authorize.anyRequest().authenticated();
 				})
 				.build();
 	}
 	
+	
+	@Bean
+	public PasswordEncoder passwordEnconder() {
+		return new BCryptPasswordEncoder(10);
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder enconder){
+		
+		UserDetails user1 = User.builder()
+				.username("usuario")
+				.password(enconder.encode("123"))
+				.roles("USER")
+				.build();
+		
+		UserDetails user2 = User.builder()
+				.username("admin")
+				.password(enconder.encode("321"))
+				.roles("ADMIN")
+				.build();
+				
+		
+		return new InMemoryUserDetailsManager(user1, user2);
+	}
 }
